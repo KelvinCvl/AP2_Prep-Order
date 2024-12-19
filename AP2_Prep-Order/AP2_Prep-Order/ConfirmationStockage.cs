7defab0cff6d16ce78ff8beb20c00c4908620b61
Kelvin <kelvinchauvel@gmail.com>
2024-12-19 12:01:51 +0100

    <hotfix> Problème d'affichage réglé
    - Affiche les pages en entier
    - Les combo box se vident à l'actionnement du bouton
    - les bonnes pages s'ouvrent depuis le menu

--- AP2_Prep-Order/AP2_Prep-Order/ConfirmationStockage.cs
+++ AP2_Prep-Order/AP2_Prep-Order/ConfirmationStockage.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,14 +19,23 @@ namespace AP2_Prep_Order
             InitializeComponent();
         }
 
-        private void ConfirmationStockage_Load(object sender, EventArgs e)
+        private void GestionHauteur_Load(object sender, EventArgs e)
         {
             cb_zone.Items.Add("Sec");
             cb_zone.Items.Add("DPH");
             cb_zone.Items.Add("Liquide");
         }
 
-        private void cb_zone_SelectedIndexChanged_1(object sender, EventArgs e)
+        private void cb_zone_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cb_remplie.Items.Clear();
+            cb_vide.Items.Clear();
+            cb_vide_fill();
+            cb_remplie_fill();
+        }
+
+        // remplir une palette 
+        public void cb_vide_fill() 
         {
             int zone;
             switch (cb_zone.SelectedItem.ToString())
@@ -43,30 +53,26 @@ namespace AP2_Prep_Order
                     MessageBox.Show("Zone inconnue", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
             }
-            StockageEtage(zone);
+            cb_vide_fill(zone, Bdd.id);
+
         }
 
-        private void StockageEtage(int zone)
+        private void cb_vide_fill(int zone, int id)
         {
             try
             {
-                using (SqlCommand command = new SqlCommand("StockageEtage", Bdd.db_connect))
+                using (SqlCommand command = new SqlCommand("PalettesVide", Bdd.db_connect))
                 {
                     command.CommandType = CommandType.StoredProcedure;
                     command.Parameters.AddWithValue("@ZoneNum", zone);
+                    command.Parameters.AddWithValue("@id", id);
 
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        lv_stock.Items.Clear();
 
                         while (reader.Read())
                         {
-                            ListViewItem item = new ListViewItem(reader["emplacement"].ToString());
-                            item.SubItems.Add(reader["allee"].ToString());
-                            item.SubItems.Add(reader["etage"].ToString());
-                            item.SubItems.Add(reader["libelleArticle"].ToString());
-                            item.SubItems.Add(reader["nombre"].ToString());
-                            lv_stock.Items.Add(item);
+                            cb_vide.Items.Add(reader["emplacement"].ToString());
                         }
                     }
                 }
@@ -75,6 +81,148 @@ namespace AP2_Prep_Order
             {
                 MessageBox.Show($"Erreur lors de la récupération des palettes : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void cb_vide_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btn_vide.Enabled = true;
+        }
+
+        private void btn_vide_Click(object sender, EventArgs e)
+        {
+            if (cb_vide.SelectedItem != null)
+            {
+                PaletteRemplir(cb_vide.SelectedItem.ToString());
+                cb_vide.Items.Clear();
+                cb_vide.SelectedIndex = -1; 
+            }
+            btn_vide.Enabled = false;
+
+            cb_vide.Text = "";
+            cb_vide.SelectedItem = null;
+            btn_vide.Enabled = false;
+
+            cb_remplie.Items.Clear();
+            cb_vide.Items.Clear();
+            cb_vide_fill();
+            cb_remplie_fill();
+        }
+
+        private void PaletteRemplir(string id)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("PaletteRemplir", Bdd.db_connect))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("emplacement", id);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la récupération des palettes : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            btn_vide.Enabled = false;  
+            cb_vide.Items.Clear();
+        }
+
+        // vider une palette
+        public void cb_remplie_fill()
+        {
+            int zone;
+            switch (cb_zone.SelectedItem.ToString())
+            {
+                case "Sec":
+                    zone = 1;
+                    break;
+                case "DPH":
+                    zone = 2;
+                    break;
+                case "Liquide":
+                    zone = 3;
+                    break;
+                default:
+                    MessageBox.Show("Zone inconnue", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+            }
+            cb_remplie_fill(zone, Bdd.id);
+        }
+
+        private void cb_remplie_fill(int zone, int id)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("PalettesRemplie", Bdd.db_connect))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@ZoneNum", zone);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            cb_remplie.Items.Add(reader["emplacement"].ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la récupération des palettes : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cb_remplie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btn_remplie.Enabled = true;
+        }
+
+        private void btn_remplie_Click(object sender, EventArgs e)
+        {
+            if (cb_remplie.SelectedItem != null)
+            {
+                PaletteVider(cb_remplie.SelectedItem.ToString());
+                cb_remplie.Items.Clear();
+                cb_remplie.SelectedIndex = -1;
+            }
+            cb_remplie.Text = "";
+            cb_remplie.SelectedItem = null;
+            btn_remplie.Enabled = false;
+
+            cb_remplie.Items.Clear();
+            cb_vide.Items.Clear();
+            cb_vide_fill();
+            cb_remplie_fill();
+        }
+
+        private void PaletteVider(string id)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("PaletteVider", Bdd.db_connect))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("emplacement", id);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la récupération des palettes : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            btn_remplie.Enabled = false;
+            cb_remplie.Items.Clear();
         }
 
     }
--- AP2_Prep-Order/AP2_Prep-Order/ConnexionCariste.cs
+++ AP2_Prep-Order/AP2_Prep-Order/ConnexionCariste.cs
@@ -24,28 +24,61 @@ namespace AP2_Prep_Order
 
         private void demandeToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            // Fermer toutes les autres pages ouvertes dans le MDI container
+            foreach (Form childForm in ConnexionCariste.ActiveForm.MdiChildren)
+            {
+                childForm.Close();
+            }
+
             FormPalettesManquantes Palettes = new FormPalettesManquantes();
             Palettes.MdiParent = this;
+
+            // Configurer la nouvelle page pour occuper tout l'espace
+            Palettes.FormBorderStyle = FormBorderStyle.None;
+            Palettes.Dock = DockStyle.Fill;
+
             Palettes.Show();
+        }
+
+        private void confirmationDeStockageToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            // Fermer toutes les autres pages ouvertes dans le MDI container
+            foreach (Form childForm in ConnexionCariste.ActiveForm.MdiChildren)
+            {
+                childForm.Close();
+            }
+
+            ConfirmationStockage Gestion = new ConfirmationStockage();
+            Gestion.MdiParent = ConnexionCariste.ActiveForm;
+
+            // Configurer la nouvelle page pour occuper tout l'espace
+            Gestion.FormBorderStyle = FormBorderStyle.None;
+            Gestion.Dock = DockStyle.Fill;
+
+            Gestion.Show();
+        }
+
+        private void gestionDuStockageEnHauteurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Fermer toutes les autres pages ouvertes dans le MDI container
+            foreach (Form childForm in ConnexionCariste.ActiveForm.MdiChildren)
+            {
+                childForm.Close();
+            }
+
+            GestionStockHauteur Confirmation = new GestionStockHauteur();
+            Confirmation.MdiParent = this;
+
+            // Configurer la nouvelle page pour occuper tout l'espace
+            Confirmation.FormBorderStyle = FormBorderStyle.None;
+            Confirmation.Dock = DockStyle.Fill;
+
+            Confirmation.Show();
         }
 
         private void ConnexionCariste_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
-        }
-
-        private void confirmationDeStockageToolStripMenuItem_Click_1(object sender, EventArgs e)
-        {
-            ConfirmationStockage Confirmation = new ConfirmationStockage();
-            Confirmation.MdiParent = this;
-            Confirmation.Show();
-        }
-
-        private void gestionDuStockageEnHauteurToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            GestionHauteur Gestion = new GestionHauteur();
-            Gestion.MdiParent = this;
-            Gestion.Show();
         }
     }
 }
