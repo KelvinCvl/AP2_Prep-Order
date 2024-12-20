da82bab1ce07e082b14b047514b8c6c7f8de36d8
QuentinHeyblom <qheyblom@outlook.com>
2024-12-20 13:11:57 +0100

    <Feature>Importer
    Fini, Fin d'AP merci à tous

--- AP2_Prep-Order/AP2_Prep-Order/ImporterResponsable.Designer.cs
+++ AP2_Prep-Order/AP2_Prep-Order/ImporterResponsable.Designer.cs
@@ -36,9 +36,9 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 42);
+            this.button1.Size = new System.Drawing.Size(216, 54);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Imorter un fichier";
+            this.button1.Text = "Imorter un fichier CSV";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
